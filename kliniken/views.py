70d90bed43d1ffe1a02b95a14f96b7bd3ec1778a
chrisgustafsson <>
2024-01-26 10:15:23 +0000

    ADD views for my bookings

--- kliniken/views.py
+++ kliniken/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Booking
 
 # Create your views here.
 
 
 def home(request):
     return render(request, "index.html")
+
+
+@login_required
+def my_bookings(request):
+    bookings = Booking.objects.filter(user=request.user)
+    return render(request, 'mybookings.html', {'bookings': bookings})
+
+
+
