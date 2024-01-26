9dab17ba20e0dbfa27c3271c3b485a591f7cc045
chrisgustafsson <>
2024-01-26 10:32:49 +0000

    DELETE commented out code views

--- kliniken/views.py
+++ kliniken/views.py
@@ -3,8 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Booking
 
-# Create your views here.
-
 
 def home(request):
     return render(request, "index.html")
