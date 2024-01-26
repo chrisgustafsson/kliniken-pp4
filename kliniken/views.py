b6e9d95dc0a999e7eaa59632d45ea6993b7fb0cc
chrisgustafsson <>
2024-01-26 11:09:07 +0000

    UPDATE imports in views

--- kliniken/views.py
+++ kliniken/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Booking
+from .forms import BookingForm, UserLoginForm, UserSignUpForm
 
 
 def index(request):
