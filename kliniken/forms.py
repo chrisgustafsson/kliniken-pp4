29d4ff9d0e29790f298caa5dfdf5dec465256a41
chrisgustafsson <>
2024-01-26 11:12:37 +0000

    ADD sign up form

--- kliniken/forms.py
+++ kliniken/forms.py
@@ -7,3 +7,9 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+class UserSignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
