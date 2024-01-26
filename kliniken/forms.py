416b19268a24c79f275a28478c70ab763b3e6745
chrisgustafsson <>
2024-01-26 11:12:05 +0000

    ADD user login form

--- kliniken/forms.py
+++ kliniken/forms.py
@@ -1,3 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Booking
+
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
